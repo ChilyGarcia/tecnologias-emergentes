afc381c3b8ee5aa9e431f22600a4388302a5a114
juan <jugada160324@gmail.com>
2024-08-25 13:05:14 -0500

    [ FEATURE ]: It is possible to implement vlaidation to add styles for button selected

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  selectedButton: string = '';
 
   constructor() {}
 
+  selectButton(button: string) {
+    this.selectedButton = button;
+  }
 }
