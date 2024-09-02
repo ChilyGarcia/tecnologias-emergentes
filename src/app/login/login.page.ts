fe04a52e42caffd0e9950b57c589cbed1ccdeb7f
JuanGarcia <82130952+ChilyGarcia@users.noreply.github.com>
2024-09-02 18:00:56 -0500

    Pagina lofin

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  constructor(private router: Router) {}
+
+  email = '';
+  password = '';
+
+  ngOnInit() {}
+
+  redirectRegister() {
+    this.router.navigate(['/pages/authentication/register']);
+  }
+  redirectToDashboard() {
+    this.router.navigate(['/pages/dashboard/home']);
+  }
+}
